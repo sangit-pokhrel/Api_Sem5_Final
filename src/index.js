e991870cbffd75a7b29f7d16be9221eb2012f6ed
sangit-pokhrel <heypsycho339@gmail.com>
2025-06-06 14:01:19 +0545

    added the database connectivity

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // src/index.js
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
 
+const { connectDB } = require("./infrastructure/database/database");
 dotenv.config();
-
 
 
 
@@ -12,9 +14,9 @@ const app = express();
 app.use(express.json());
 
 
-
 mongoose.connect(process.env.MONGO_URI).then(() => {
   app.listen(process.env.PORT || 5000, () =>
     console.log(`✅ Server running on port ${process.env.PORT}`)
   );
+  connectDB();
 });
---
+++ src/infrastructures/database/database.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log("MongoDB disconnected");
+  } catch (error) {
+    console.error("MongoDB disconnection failed:", error.message);
+  }
+};
+
+module.exports = {
+  connectDB,
+  disconnectDB,
+};
